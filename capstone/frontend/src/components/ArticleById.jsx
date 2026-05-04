41cc327dbe2ef3b58753bfda83970a25ac22fd1a
shivathota1323 <shivathota1323@gmail.com>
2026-05-04 22:28:30 +0530

    Delete capstone directory

--- capstone/frontend/src/components/ArticleById.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ArticleById() {
-  return (
-    <div>ArticleById</div>
-  )
-}
-
-export default ArticleById
--- capstone/frontend/src/components/WriteArticle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function WriteArticle() {
-  return (
-    <div>WriteArticle</div>
-  )
-}
-
-export default WriteArticle
